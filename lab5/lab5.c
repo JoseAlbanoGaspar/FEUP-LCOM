145a0d97d007cf041faf7f37585177b5577981b6
Anibal <anibal.pereira.neves.ferreira@gmail.com>
2022-04-19 11:45:55 +0100

    Added some code to first function

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -2,9 +2,40 @@
 #include <lcom/lcf.h>
 
 #include <lcom/lab5.h>
+//#include <machine/int86.h>
 
 #include <stdint.h>
 #include <stdio.h>
+//#include "register.c"
+
+/* Constants for VBE 0x105 mode */
+
+/* The physical address may vary from VM to VM.
+ * At one time it was 0xD0000000
+ *  #define VRAM_PHYS_ADDR    0xD0000000 
+ * Currently on lab B107 is 0xF0000000
+ * Better run my version of lab5 as follows:
+ *     service run `pwd`/lab5 -args "mode 0x105"
+ */
+#define VRAM_PHYS_ADDR	0xF0000000
+#define H_RES             1024
+#define V_RES		  768
+#define BITS_PER_PIXEL	  8
+
+/* Private global variables */
+
+//static char *video_mem;		/* Process address to which VRAM is mapped */
+
+//static unsigned h_res = H_RES;		/* Horizontal screen resolution in pixels */
+//static unsigned v_res = V_RES;		/* Vertical screen resolution in pixels */
+//static unsigned bits_per_pixel = BITS_PER_PIXEL; /* Number of VRAM bits per pixel */
+
+
+//Timer to be incremented by the timer interrupts
+extern int count;
+//Hook id to be used to set the interrupt policy
+extern int hook_id_timer;
+int hook_id = 1;
 
 // Any header files included below this line should have been created by you
 
@@ -33,9 +64,61 @@ int main(int argc, char *argv[]) {
 }
 
 int(video_test_init)(uint16_t mode, uint8_t delay) {
-  /* To be completed */
-  printf("%s(0x%03x, %u): under construction\n", __func__, mode, delay);
+  reg86_t reg86;
 
+  reg86.intno = 0x10;
+  reg86.ah = 0x4F;   
+  reg86.al = 0x02;    
+  reg86.bx = mode;
+  printf("%x", reg86.bx);
+  sys_int86(&reg86);
+  //if( sys_int86(&reg86) != OK ) {
+   // printf("\tvg_exit(): sys_int86() failed \n");
+   // return 1;
+  //}
+  
+  uint32_t irq_set = BIT(hook_id);
+  uint32_t irq_set_timer = BIT(hook_id_timer);
+  uint8_t aux = (uint8_t)hook_id;
+  hook_id = (int)aux;
+  aux = (uint8_t)hook_id_timer;
+  if(timer_subscribe_int(&aux))
+    return 1;
+  hook_id_timer = (int)aux;
+  int ipc_status;
+  message msg;
+  //1 is true
+  int r;
+  while (count < (int) delay * 60) {
+
+    // Get a request message
+    if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+      printf("driver_receive failed with: %d", r);
+      continue;
+    }
+    if (is_ipc_notify(ipc_status)) { // received notification
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE:
+
+          // hardware interrupt notification
+          if (msg.m_notify.interrupts & irq_set) { // subscribed interrupt
+            count = 0;
+          }
+          if(msg.m_notify.interrupts & irq_set_timer){
+            timer_int_handler();
+          }
+          break;
+
+        default:
+          break; // no other notifications expected: do nothing
+      }
+    }
+    else { //received a standard message, not a notification
+      // no standard messages expected: do nothing
+    }
+  }
+  timer_unsubscribe_int();
+  vg_exit();
   return 1;
 }
 
@@ -78,3 +161,4 @@ int(video_test_controller)() {
 
   return 1;
 }
+
