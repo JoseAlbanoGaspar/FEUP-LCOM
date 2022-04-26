0021132393ab958524200a248c22eb0ff890fdb5
Jose Gaspar <up202008561@fe.up.pt>
2022-04-26 17:40:42 +0100

    2_function printing things

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,43 +1,19 @@
 // IMPORTANT: you must include the following line in all your C files
 #include <lcom/lcf.h>
-
 #include <lcom/lab5.h>
-
 #include <stdint.h>
 #include <stdio.h>
-<<<<<<< HEAD
+#include <vbe.h>
+//#include <keyboard.h>
+#include <lcom/timer.h>
+#include <i8254.h>
 
-
-/* Constants for VBE 0x105 mode */
-
-/* The physical address may vary from VM to VM.
- * At one time it was 0xD0000000
- *  #define VRAM_PHYS_ADDR    0xD0000000 
- * Currently on lab B107 is 0xF0000000
- * Better run my version of lab5 as follows:
- *     service run `pwd`/lab5 -args "mode 0x105"
- */
-
-char *video_mem;
-unsigned h_res;
-unsigned v_res;
-unsigned bits_per_pixel;
-unsigned bytes_per_pixel;
-
-=======
-#include "int86.h"
-
-static char *video_mem;		/* Process address to which VRAM is mapped */
-
-static unsigned h_res;		/* Horizontal screen resolution in pixels */
-static unsigned v_res;		/* Vertical screen resolution in pixels */
-static unsigned bits_per_pixel; /* Number of VRAM bits per pixel */
->>>>>>> d95e263685d24b42aeb9a7309246d958e8178336
-
+unsigned int counter_global = 0;
 
 // Any header files included below this line should have been created by you
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   // sets the language of LCF messages (can be either EN-US or PT-PT)
   lcf_set_language("EN-US");
 
@@ -61,55 +37,42 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-int(video_test_init)(uint16_t mode, uint8_t delay) {
+int(video_test_init)(uint16_t mode, uint8_t delay)
+{
+  /* Initialize Graphics Mode */
+  vg_init(mode);
 
-  video_mem = vg_init(mode);
-  
-  tickdelay(micros_to_ticks(delay * 1000000));
+  /* Waits delay */
+  sleep(delay);
+
+  /* Exit Graphics Mode */
   vg_exit();
+
   return 0;
 }
 
 int(video_test_rectangle)(uint16_t mode, uint16_t x, uint16_t y,
-                          uint16_t width, uint16_t height, uint32_t color) {
-  
+                          uint16_t width, uint16_t height, uint32_t color)
+{
+  /* Initialize Graphics Mode */
   vg_init(mode);
 
-  vg_draw_rectangle(x,y,width,height,color);
-  printf("AFTER IT'S DONE\n");
-  tickdelay(micros_to_ticks(2000000));
+  /* Draw Rectangle */
+  vg_draw_rectangle(x, y, width, height, color);
+  sleep(2);
+
+  
   vg_exit();
-  return 1;
+
+  return 0;
 }
 
-int(video_test_pattern)(uint16_t mode, uint8_t no_rectangles, uint32_t first, uint8_t step) {
-  /* To be completed */
-  printf("%s(0x%03x, %u, 0x%08x, %d): under construction\n", __func__,
-         mode, no_rectangles, first, step);
-
-  return 1;
+int(video_test_pattern)(uint16_t mode, uint8_t no_rectangles, uint32_t first, uint8_t step)
+{
+  return 0;
 }
 
-int(video_test_xpm)(xpm_map_t xpm, uint16_t x, uint16_t y) {
-  /* To be completed */
-  printf("%s(%8p, %u, %u): under construction\n", __func__, xpm, x, y);
-
-  return 1;
+int test_controller()
+{
+ return 0;
 }
-
-int(video_test_move)(xpm_map_t xpm, uint16_t xi, uint16_t yi, uint16_t xf, uint16_t yf,
-                     int16_t speed, uint8_t fr_rate) {
-  /* To be completed */
-  printf("%s(%8p, %u, %u, %u, %u, %d, %u): under construction\n",
-         __func__, xpm, xi, yi, xf, yf, speed, fr_rate);
-
-  return 1;
-}
-
-int(video_test_controller)() {
-  /* To be completed */
-  printf("%s(): under construction\n", __func__);
-
-  return 1;
-}
-
