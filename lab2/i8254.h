94cfcbfcf20485064b66c26a7517e045fbd2036f
Anibal <anibal.pereira.neves.ferreira@gmail.com>
2022-05-03 10:49:22 +0100

    as

---
+++ lab2/i8254.h
@@ -0,0 +1,70 @@
+#ifndef _LCOM_I8254_H_
+#define _LCOM_I8254_H_
+
+#include <lcom/lcf.h>
+
+/** @defgroup i8254 i8254
+ * @{
+ * Constants for programming the i8254 Timer. Needs to be completed.
+ */
+
+//todos os timer usam o mesmo "clock signal", com frequência = 1193181 Hz
+#define TIMER_FREQ 1193181 /**< @brief clock frequency for timer in PC and AT */
+#define TIMER0_IRQ 0 /**< @brief Timer 0 IRQ line */
+
+
+
+
+/* I/O port addresses */
+
+#define TIMER_0    0x40 /**< @brief Timer 0 count register */
+#define TIMER_1    0x41 /**< @brief Timer 1 count register */
+#define TIMER_2    0x42 /**< @brief Timer 2 count register */
+#define TIMER_CTRL 0x43 /**< @brief Control register */
+
+//acho que SPEAKER_CTRL não é usado aqui
+#define SPEAKER_CTRL 0x61 /**< @brief Register for speaker control  */
+
+
+
+
+/* Timer control word*/
+
+/* Timer selection: bits 7 and 6 */
+
+#define TIMER_SEL0   0x00              /**< @brief Control Word for Timer 0 */ //ambos os bits 6 e 7 a 0
+#define TIMER_SEL1   BIT(6)            /**< @brief Control Word for Timer 1 */ //bit 6 a 1
+#define TIMER_SEL2   BIT(7)            /**< @brief Control Word for Timer 2 */ //bit 7 a 1
+
+/* Register selection: bits 5 and 4 */
+
+#define TIMER_LSB     BIT(4)                  /**< @brief Initialize Counter LSB only */ //bit 4 a 1
+#define TIMER_MSB     BIT(5)                  /**< @brief Initialize Counter MSB only */ //bit 5 a 1
+#define TIMER_LSB_MSB (TIMER_LSB | TIMER_MSB) /**< @brief Initialize LSB first and MSB afterwards */ //ambos os bits 5 e 4 a 1
+
+/* Operating mode: bits 3, 2 and 1 */
+
+#define TIMER_SQR_WAVE (BIT(2) | BIT(1)) /**< @brief Mode 3: square wave generator */ //ambos os bits 2 e 1 a 1
+#define TIMER_RATE_GEN BIT(2)            /**< @brief Mode 2: rate generator */ //bit 2 a 1
+
+/* Counting mode: bit 0 */
+
+#define TIMER_BCD 0x01 /**< @brief Count in BCD */ //bit 1 a 1
+#define TIMER_BIN 0x00 /**< @brief Count in binary */ //tudo a 0
+
+
+
+
+/* READ-BACK COMMAND FORMAT */
+
+#define TIMER_RB_COUNT_  BIT(5) //active low
+#define TIMER_RB_STATUS_ BIT(4) //active low
+#define TIMER_RB_SEL(n)  BIT((n) + 1) //escolha dos timers através da tabela do RB command
+//exemplo: timer 0 tem de ter o bit 1 a 1, logo TIMER_RB_SEL(0) -> bit(0+1) = 1 = timer 0 selecionado
+
+#define TIMER_RB_CMD (BIT(7) | BIT(6)) /**< @brief Read Back Command */ //ambos os bits 6 e 7 a 1
+
+
+/**@}*/
+
+#endif /* _LCOM_I8254_H */
---
+++ lab2/utils.c
@@ -0,0 +1,41 @@
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+
+int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
+<<<<<<< HEAD
+  *lsb = (uint8_t) (val & 0x00FF);
+=======
+  val = (0x00FF & val);
+  *lsb = (uint8_t) val; //lsb fica com os 8 bits menos significativos de val
+>>>>>>> 0a5f24c698faf98c8241aff8609b31dd625e802e
+  return 0;
+}
+
+int(util_get_MSB)(uint16_t val, uint8_t *msb) {
+<<<<<<< HEAD
+  *msb = (uint8_t) (val >> 8) & 0x00FF;
+=======
+  /* To be implemented by the students */
+  val = (val & 0xFF00);
+  *msb = val >> 8;
+>>>>>>> 0a5f24c698faf98c8241aff8609b31dd625e802e
+  return 0;
+}
+
+int (util_sys_inb)(int port, uint8_t *value) {
+<<<<<<< HEAD
+  uint32_t value2;
+  if (sys_inb(port, &value2) != OK) return 1;
+  *value = value2 & 255; 
+  return 0;
+=======
+  uint32_t value2; //temporary value for the return of the sys_inb funtion
+  if (sys_inb(port, &value2) == OK) {
+    *value = value2 & 255/*ou & 0xFF*/; //*value = (uint8_t)value2;
+    return 0; //success
+  }
+  printf("Error in util_sys_inb\n");
+  return 1; //error
+>>>>>>> 0a5f24c698faf98c8241aff8609b31dd625e802e
+}
