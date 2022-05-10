655df84f04c0158e97de3a3763e8eb69a21c484f
Anibal <up202005429@g.uporto.pt>
2022-05-10 16:28:16 +0100

    added files for lab4

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -113,30 +113,6 @@ int(video_test_rectangle)(uint16_t mode, uint16_t x, uint16_t y,
 
 int(video_test_pattern)(uint16_t mode, uint8_t no_rectangles, uint32_t first, uint8_t step)
 {
-<<<<<<< HEAD
-  /* Initialize Graphics Mode */
-  vg_init(mode);
-
-  uint32_t color;
-  int x = 0, y = 0;
-
-  for(int i = 0; i < (int) no_rectangles; i++){
-    if (mode == 0x0105){
-      index(row, col) = (first + (row * no_rectangles + col) * step) % (1 << bitsPerPixel);
-    }
-    else if (mode == 0x0115){
-      
-    }
-    color = first + (step * i);
-    
-    vg_draw_rectangle(x, y, 100, 100, color);
-  }
-  
-  
-  /* Wait for ESC key */
-  //Here we select the bit in the hook_id needed to check if we got the right interruption
-  uint32_t irq_set = BIT(hook_id);
-=======
   int ipc_status;
   message msg;
   uint32_t irq_set = BIT(1);
@@ -152,47 +128,12 @@ int(video_test_pattern)(uint16_t mode, uint8_t no_rectangles, uint32_t first, ui
   }
 
   /* Keyboard Subscribe With Bit_No = 1 */
->>>>>>> 68172e1a4b672b27bd189153fa551af3c3087b61
   uint8_t aux = (uint8_t)hook_id;
 
   //Subscription of the interruption
   if(kbd_subscribe_int(&aux))
     return 1;
 
-<<<<<<< HEAD
-  hook_id = (int)aux;
-//--------------------------------
-  int ipc_status;
-  message msg;
-  //1 is true
-  int r;
-
-  while (scancode != ESC_KEY) {
-    // Get a request message
-    if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
-      printf("driver_receive failed with: %d", r);
-      continue;
-    }
-    if (is_ipc_notify(ipc_status)) { // received notification
-      switch (_ENDPOINT_P(msg.m_source)) {
-        case HARDWARE:
-
-          // hardware interrupt notification
-          if (msg.m_notify.interrupts & irq_set) { // subscribed interrupt
-            kbc_ih();
-          }
-          break;
-        default:
-          break; // no other notifications expected: do nothing
-      }
-    }
-    else { //received a standard message, not a notification
-      // no standard messages expected: do nothing
-    }
-  }
-  kbd_unsubscribe_int();
-  vg_exit();
-=======
   hook_id = (int)aux; 
   /* Device Driver Loop Start */
   while (scancode != ESC_KEY)
@@ -233,19 +174,10 @@ int(video_test_pattern)(uint16_t mode, uint8_t no_rectangles, uint32_t first, ui
   /* Exit Graphics Mode */
   vg_exit();
 
->>>>>>> 68172e1a4b672b27bd189153fa551af3c3087b61
   return 0;
 }
 
 int test_controller()
 {
  return 0;
-}
-
-int video_test_xpm(const char *xpm[], uint16_t x, uint16_t y){
-  return 0;
-}
-
-int video_test_move(const char *xpm[], uint16_t xi, uint16_t yi, uint16_t xf, uint16_t yf, int16_t speed, uint8_t fr_rate){
-  return 0;
 }
